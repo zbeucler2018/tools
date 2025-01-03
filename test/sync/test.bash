cf4e51ff9db8cf938fa174c7f8b29dfe4d5c14d8
zackbeucler <zbeucler@gmail.com>
2025-01-02 22:59:13 -0500

    convert sync to cli and add some minor improvements

--- test/sync/test.bash
+++ test/sync/test.bash
@@ -7,21 +7,21 @@ docker compose up -d
 
 sleep 10
 
-echo "SLAVE TABLES..."
-docker exec -it slave mysql testdb -u admin -padmin -e 'SHOW TABLES;'
+echo "REPLICA TABLES..."
+docker exec -it replica mysql testdb -u admin -padmin -e 'SHOW TABLES;'
 echo "MASTER TABLES..."
 docker exec -it master mysql testdb -u admin -padmin -e 'SHOW TABLES;'
 
-echo "SLAVE DATA..."
-docker exec -it slave mysql testdb -u admin -padmin -e 'INSERT INTO table1 (timestamp, col1) VALUES (1, 0), (2, 0);'
-docker exec -it slave mysql testdb -u admin -padmin -e 'SELECT * FROM table1;'
+echo "REPLICA DATA..."
+docker exec -it replica mysql testdb -u admin -padmin -e 'INSERT INTO table1 (timestamp, col1) VALUES (1, 0), (2, 0);'
+docker exec -it replica mysql testdb -u admin -padmin -e 'SELECT * FROM table1;'
 
 echo "MASTER DATA..."
 docker exec -it master mysql testdb -u admin -padmin -e 'INSERT INTO table1 (timestamp, col1) VALUES (1, 0), (2, 0), (3, 0), (4, 0);'
 docker exec -it master mysql testdb -u admin -padmin -e 'SELECT * FROM table1;'
 
-python3.10 ./../../tools/sync.py
-echo "SLAVE DATA..."
-docker exec -it slave mysql testdb -u admin -padmin -e 'SELECT * FROM table1;'
+python3.10 ./../../tools/sync.py -mh '0.0.0.0' -mu admin -mpw admin -mdb testdb -mp 3306 -rh '0.0.0.0' -ru admin -rpw admin -rdb testdb -rp 3307 
+echo "REPLICA DATA..."
+docker exec -it replica mysql testdb -u admin -padmin -e 'SELECT * FROM table1;'
 
 docker compose down
--- tools/sync.py
+++ tools/sync.py
@@ -1,28 +1,19 @@
+import argparse
+
 import mysql.connector
 from mysql.connector import Error, MySQLConnection
 
 
-# Database configurations
-MASTER_DB = {
-    'host': '0.0.0.0',
-    'user': 'admin',
-    'password': 'admin',
-    'database': 'testdb',
-    'compress': True,
-    'port': 3306
-}
 
-SLAVE_DB = {
-    'host': '0.0.0.0',
-    'user': 'admin',
-    'password': 'admin',
-    'database': 'testdb',
-    'compress': True,
-    'port': 3307
-}
+def get_table_names(connection: MySQLConnection) -> list[str]:
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SHOW TABLES")
+            return cursor.fetchall()[0]
+    except Error as e:
+        print(f"Error getting table names: {e}")
+        return
 
-BATCH_SIZE = 2
-assert BATCH_SIZE > 1, f"BATCH_SIZE needs to be bigger than 1 not {BATCH_SIZE}"
 
 def connect_to_database(config) -> MySQLConnection:
     try:
@@ -32,19 +23,20 @@ def connect_to_database(config) -> MySQLConnection:
         print(f"Error connecting to database: {e}")
         return
 
-def sync_table(master_conn: MySQLConnection, slave_conn: MySQLConnection, table: str):
+
+def sync_table(master_conn: MySQLConnection, replica_conn: MySQLConnection, table: str):
     try:
-        # master has up-to-date data, slave is out-of-date
-        with master_conn.cursor(dictionary=True) as master_cursor, slave_conn.cursor() as slave_cursor:
-            # get most recent slave timestamp
-            slave_cursor.execute(f"SELECT MAX(timestamp) FROM {table}")
-            slave_latest = slave_cursor.fetchone()[0] or "0"
+        # master has up-to-date data, replica is out-of-date
+        with master_conn.cursor(dictionary=True) as master_cursor, replica_conn.cursor() as replica_cursor:
+            # get most recent replica timestamp
+            replica_cursor.execute(f"SELECT MAX(timestamp) FROM {table}")
+            replica_latest = replica_cursor.fetchone()[0] or "0"
 
             # fetch missing data
-            master_cursor.execute(f"SELECT * FROM {table} WHERE timestamp > %s", (slave_latest,))
-
+            master_cursor.execute(f"SELECT * FROM {table} WHERE timestamp > %s", (replica_latest,))
+            batch_size = 10_000
             while True:
-                missing_data = master_cursor.fetchmany(BATCH_SIZE)
+                missing_data = master_cursor.fetchmany(batch_size)
                 if not missing_data:
                     print("Done syncing")
                     break
@@ -55,32 +47,90 @@ def sync_table(master_conn: MySQLConnection, slave_conn: MySQLConnection, table:
                 for row in missing_data:
                     data = tuple(row.values())
                     print("Executing: ", query, data)
-                    slave_cursor.execute(query, data)
-                slave_conn.commit()
+                    replica_cursor.execute(query, data)
+                replica_conn.commit()
     except Error as e:
         print(f"Error syncing table {table}: {e}")
 
 
-def main():
-    master_conn = connect_to_database(MASTER_DB)
-    slave_conn = connect_to_database(SLAVE_DB)
+def handle_args():
+    parser = argparse.ArgumentParser(description="sync databases the hard way")
+    parser.add_argument("-mh", "--master_host", type=str, help="IP address or hostname of the master db")
+    parser.add_argument("-mu", "--master_user", type=str, help="SQL user to use on the master db")
+    parser.add_argument("-mpw", "--master_pass", type=str, help="Password to the SQL user to use on the master db")
+    parser.add_argument("-mdb", "--master_db", type=str, help="Name of the database to sync to")
+    parser.add_argument("-mp", "--master_port", type=int, default=3306, help="Port to connect to the master db")
     
-    if not master_conn or not slave_conn:
-        print(f"Missing connection. master={master_conn} slave={slave_conn}")
+    parser.add_argument("-rh", "--replica_host", type=str, help="IP address or hostname of the replica db")
+    parser.add_argument("-ru", "--replica_user", type=str, help="SQL user to use on the replica db")
+    parser.add_argument("-rpw", "--replica_pass", type=str, help="Password to the SQL user to use on the replica db")
+    parser.add_argument("-rdb", "--replica_db", type=str, help="Name of the database to sync")
+    parser.add_argument("-rp", "--replica_port", type=int, default=3306, help="Port to connect to the replica db")
+    return parser.parse_args()
+
+
+def main():
+    args = handle_args()
+
+    master_db_config = {
+        "host": args.master_host,
+        "user": args.master_user,
+        "password": args.master_pass,
+        "database": args.master_db,
+        "port": args.master_port,
+        "compress": True
+    }
+    replica_db_config = {
+        "host": args.replica_host,
+        "user": args.replica_user,
+        "password": args.replica_pass,
+        "database": args.replica_db,
+        "port": args.replica_port,
+        "compress": True
+    }
+
+    # master_db_config = {
+    #     'host': '0.0.0.0',
+    #     'user': 'admin',
+    #     'password': 'admin',
+    #     'database': 'testdb',
+    #     'compress': True,
+    #     'port': 3306
+    # }
+
+    # replica_db_config = {
+    #     'host': '0.0.0.0',
+    #     'user': 'admin',
+    #     'password': 'admin',
+    #     'database': 'testdb',
+    #     'compress': True,
+    #     'port': 3307
+    # }
+    
+    print("Master Connection Config")
+    print(*master_db_config.items(), sep="\n")
+    print("Replica Connection Config")
+    print(*replica_db_config.items(), sep="\n")
+
+    master_conn = connect_to_database(master_db_config)
+    replica_conn = connect_to_database(replica_db_config)
+    
+    if master_conn is None or replica_conn is None:
+        print(f"Missing connection. master={master_conn} replica={replica_conn}")
         master_conn.close()
-        slave_conn.close()
+        replica_conn.close()
         return
     
     try:
-        tables_to_sync = ['table1']
+        tables_to_sync = get_table_names(master_conn)
         for table in tables_to_sync:
-            sync_table(master_conn, slave_conn, table)
-    
+            sync_table(master_conn, replica_conn, table)
+
     finally:
-        if master_conn.is_connected():
+        if master_conn.is_connected() and not master_conn is None:
             master_conn.close()
-        if slave_conn.is_connected():
-            slave_conn.close()
+        if replica_conn.is_connected() and not replica_conn is None:
+            replica_conn.close()
 
 if __name__ == "__main__":
     main()
